c6746213a686e720795455f3f3190bf0935e11d7
deivisQuin <deivis.quin@hotmail.com>
2020-05-31 12:18:30 -0500

    vista nuevo estado

---
+++ resources/views/nuevoEstadoGracias.blade.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <title>Pago Libre</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{url('estilo/bootstrap4/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('estilo/css/all.min.css')}}">
+</head>
+<body>
+	<div class="container panel panel-default">	
+	<br>
+	<div class="container">
+		<div class="card">
+			<div class="card-header">
+				<h3>Estados de Cliente y Comercio</h3>
+			</div>
+			@if(isset($aTransaccion))
+			<div class="card-body">
+				<div class="form-group">
+					<label for = "Password"><strong>Cliente:</strong></label>
+					{{$aTransaccion->transaccionClienteEstado == 1 ? "Pendiente de Recepción" : "Producto o Servicio Recibido "}}
+					@if($aTransaccion->transaccionClienteEstado == 1)
+						<i class="fas fa-clock"></i>
+					@else
+						<i class="fas fa-thumbs-up"></i>
+					@endif
+				</div><br>
+				<div class="form-group">
+					<label for = "Password"><strong>Comercio:</strong></label>
+					{{$aTransaccion->transaccionComercioEstado == 1 ? "Pendiente de Entrega" : "Producto o Servicio Entregado" }}
+					@if($aTransaccion->transaccionComercioEstado == 1)
+						<i class="fas fa-shipping-fast"></i>
+					@else
+						<i class="fas fa-check"></i>
+					@endif
+				</div>
+			</div>
+			@endif
+		</div>
+	</div>
+</body>
