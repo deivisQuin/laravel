de9a7a55c97edc5af8cdce596923cfb2ba7a64c9
deivisQuin <deivis.quin@hotmail.com>
2020-10-06 00:46:24 -0500

    se envia el id de la transaccion

--- app/Mail/MessageReceived.php
+++ app/Mail/MessageReceived.php
@@ -58,9 +58,9 @@ class MessageReceived extends Mailable
             //$imagen = $qrCode->size(200)->generate("hola");
             //$imagen = $qrCode->format('png')->merge('https://image.flaticon.com/icons/png/512/838/838608.png', .3, true)->size(200)->generate("hola");
             //$imagen = \QrCode::format("png")->size(200)->generate('https://comparadordeventas.com/pagolibre/public/nuevoEstado/13/$2y$10$TvsMCB0tPCqq8OUmVfMAc.EBc7gK0S88AQiCSiiEcYamlz93VXLFe/1', '../public/qrcodes/15.png');
-            $imagen = \QrCode::format("png")->size(200)->generate('https://comparadordeventas.com/pagolibre/public/nuevoEstado/$this->transaccionId/$this->transaccionComercioClientePasswordLink/1', '../public/qrcodes/$this->transaccionId.png');
-            
-            $this->view('emails.mensajeConfirmacionQR', compact("imagen"))->subject("Pago Libre - Confirmar Recibimiento del Pedido");
+            $imagen = \QrCode::format("png")->size(200)->generate('https://comparadordeventas.com/pagolibre/public/nuevoEstado/'.$this->transaccionId.'/'.$this->transaccionComercioClientePasswordLink.'/1', '../public/qrcodes/'.$this->transaccionId.'.png');
+            $imagenId = $this->transaccionId;
+            $this->view('emails.mensajeConfirmacionQR', compact("imagenId"))->subject("Pago Libre - Confirmar Recibimiento del Pedido");
         }
     }
 }
