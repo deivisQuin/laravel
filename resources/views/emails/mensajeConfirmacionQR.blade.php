3b1a33be108c63b0643e7c516ed86d82b57abf9b
deivisQuin <deivis.quin@hotmail.com>
2020-10-06 00:49:09 -0500

    cambio de nombre en la variable

--- resources/views/emails/mensajeConfirmacionQR.blade.php
+++ resources/views/emails/mensajeConfirmacionQR.blade.php
@@ -20,7 +20,7 @@ hola el siguiente es el códigoQR:
     <!--<img src="{{asset('qrcodes/qrcode.svg')}}" alt = "Si no se visualiza el codigo QR descargalo desde:">-->
     
     <!--<img src="{{asset('imagen/cargador_nuevo_sol.gif')}}">-->
-    <img src="{{asset('qrcodes/$imagenId.png')}}">
+    <img src="{{asset('qrcodes/$imagen.png')}}">
     
 
     <!--{!! QrCode::size(250)->generate('www.google.com'); !!} -->
