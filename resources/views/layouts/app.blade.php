394d04e8e69e6f1133ad0f055d08794b6cbbcc05
deivisQuin <deivis.quin@hotmail.com>
2020-10-06 02:31:35 -0500

    se elimina codigoqr del footer que era de rpueba

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,9 +104,6 @@
     </div>
     
         @yield('js')
-        <div class="visible-print text-center">
-            {!! QrCode::size(100)->generate(Request::url()); !!}
-            <p>Escanéame para volver a la página principal.</p>
-        </div>
+        
 </body>
 </html>
