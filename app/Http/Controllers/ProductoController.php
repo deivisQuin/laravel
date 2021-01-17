5169b09eba2bbe5991cb9174dd09c36ba4f5e908
= <=>
2021-01-17 03:03:20 -0500

    Se envía el listado de los productos en los locales de manera ordenada y ascendente

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -52,7 +52,7 @@ class ProductoController extends Controller
             //Se obtiene el local de la empresa
             $aLocal = Local::where("localEmpresaId", "=", $oEmpresa->empresaId)->where("localEstadoId", "=", 1)->get();
             //Se obtiene el listado de productos del local
-            $aProducto = LocalLineaSublineaProducto::where("LLSPLocalId", "=", $aLocal[0]->localId)->where("LLSPEstadoId", "=", 1)->get();
+            $aProducto = LocalLineaSublineaProducto::where("LLSPLocalId", "=", $aLocal[0]->localId)->where("LLSPEstadoId", "=", 1)->orderBy('LLSPPosicion', 'Asc')->get();
             
             //Se obtiene los lugares de delivery de la empresa
             $aLocalUbigeoDelivery = LocalUbigeoDelivery::where("LULocalId", "=", $aLocal[0]->localId)->get();
